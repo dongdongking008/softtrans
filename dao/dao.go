9f57d22b2c8c3f08f525205f2726a0c7274affd5
陈小东 <xiaodong.chen@mtime.com>
2018-08-09 14:52:35 +0800

    refactor dao

--- dao/dao.go
+++ dao/dao.go
@@ -1,22 +1,24 @@
 package dao
 
 import (
-	"errors"
-	"time"
-	"github.com/cuigh/auxo/config"
-	"github.com/cuigh/auxo/util/lazy"
+	"github.com/cuigh/auxo/db/mongo"
 	"github.com/cuigh/auxo/log"
+	"github.com/cuigh/auxo/util/lazy"
 	"github.com/globalsign/mgo"
-	"github.com/dongdongking008/softtrans/misc"
+)
+
+const (
+	DBName  = "tcc"
+	PKGNAME = "softtrans.dao"
 )
 
 var (
 	indexes = map[string][]mgo.Index{
 		"transaction": {
 			mgo.Index{Key: []string{"trans_id"}, Unique: true},
-			mgo.Index{Key: []string{"status"}, },
-			mgo.Index{Key: []string{"enter_time"}, },
-			mgo.Index{Key: []string{"expire_time"}, },
+			mgo.Index{Key: []string{"status"}},
+			mgo.Index{Key: []string{"enter_time"}},
+			mgo.Index{Key: []string{"expire_time"}},
 			mgo.Index{Key: []string{"lu_time"}},
 		},
 	}
@@ -34,81 +36,36 @@ func Get() (*Dao, error) {
 	return v.(*Dao), nil
 }
 
-type database struct {
-	db *mgo.Database
-}
-
-func (d *database) Close() {
-	d.db.Session.Close()
-}
-
-func (d *database) C(name string) *mgo.Collection {
-	return d.db.C(name)
-}
-
-func (d *database) Run(cmd, result interface{}) error {
-	return d.db.Run(cmd, result)
-}
-
 type Dao struct {
-	logger  log.Logger
-	session *mgo.Session
+	dbName string
+	logger log.Logger
 }
 
-func New(addr string) (*Dao, error) {
-	if addr == "" {
-		return nil, errors.New("database address must be configured for mongo storage")
-	}
-
-	s, err := mgo.DialWithTimeout(addr, time.Second*5)
-	if err != nil {
-		return nil, err
-	}
-
-	d := &Dao{
-		session: s,
-		logger:  log.Get("mongo"),
-	}
-	return d, nil
-}
-
-func (d *Dao) Init() {
-	db := d.db()
-	defer db.Close()
-
-	for name, ins := range indexes {
-		c := db.C(name)
-		for _, in := range ins {
-			err := c.EnsureIndex(in)
-			if err != nil {
-				d.logger.Warnf("Ensure index %s-%v failed: %v", name, in.Key, err)
+func (d *Dao) Init() error {
+	return d.do(func(db mongo.DB) error {
+		for name, ins := range indexes {
+			c := db.C(name)
+			for _, in := range ins {
+				err := c.EnsureIndex(in)
+				if err != nil {
+					d.logger.Warnf("Ensure index %s-%v failed: %v", name, in.Key, err)
+				}
 			}
 		}
-	}
+		return nil
+	})
 }
 
-func (d *Dao) Close() {
-	d.session.Close()
-}
-
-func (d *Dao) db() *database {
-	return &database{
-		db: d.session.Copy().DB(""),
-	}
-}
-
-func (d *Dao) do(fn func(db *database)) {
-	db := d.db()
-	defer db.Close()
-
-	fn(db)
+func (d *Dao) do(fn func(db mongo.DB) error) error {
+	return mongo.With(d.dbName, fn)
 }
 
 func create() (interface{}, error) {
-	d, err := New(config.GetString(misc.KeyDBAddress))
-
-	if err == nil {
-		d.Init()
+	d := &Dao{
+		dbName: DBName,
+		logger: log.Get(PKGNAME),
 	}
-	return d, err
+
+	e := d.Init()
+	return d, e
 }
--- dao/transaction.go
+++ dao/transaction.go
@@ -1,54 +1,59 @@
 package dao
 
 import (
+	"fmt"
+	"github.com/cuigh/auxo/db/mongo"
+	"github.com/cuigh/auxo/errors"
+	"github.com/dongdongking008/softtrans/contract"
 	"github.com/dongdongking008/softtrans/model"
 	"github.com/globalsign/mgo"
 	"github.com/globalsign/mgo/bson"
-	"github.com/cuigh/auxo/errors"
-	"github.com/dongdongking008/softtrans/contract"
-	"fmt"
 	"time"
 )
 
 func (d *Dao) TransGet(transUniqId string) (trans *model.Transaction, err error) {
-	d.do(func(db *database) {
+	err = d.do(func(db mongo.DB) error {
 		trans = &model.Transaction{}
 
 		q := bson.M{
 			"_id": transUniqId,
 		}
-		err = db.C("transaction").Find(q).One(trans)
-		if err == mgo.ErrNotFound {
-			trans, err = nil, nil
+		errDB := db.C("transaction").Find(q).One(trans)
+		if errDB == mgo.ErrNotFound {
+			trans = nil
+			return errDB
 		} else if err != nil {
 			trans = nil
 		}
+		return errDB
 	})
 	return
 }
 
 func (d *Dao) TransGetByTransId(id *model.TransactionId) (trans *model.Transaction, err error) {
-	d.do(func(db *database) {
+	err = d.do(func(db mongo.DB) error {
 		trans = &model.Transaction{}
 
 		q := bson.M{
-			"trans_id.app_id": id.AppId,
+			"trans_id.app_id":   id.AppId,
 			"trans_id.bus_code": id.BusCode,
-			"trans_id.trx_id": id.TrxId,
+			"trans_id.trx_id":   id.TrxId,
 		}
-		err = db.C("transaction").Find(q).One(trans)
-		if err == mgo.ErrNotFound {
-			trans, err = nil, nil
-		} else if err != nil {
+		errDB := db.C("transaction").Find(q).One(trans)
+		if errDB == mgo.ErrNotFound {
+			trans = nil
+			return nil
+		} else if errDB != nil {
 			trans = nil
 		}
+		return errDB
 	})
 	return
 }
 
 func (d *Dao) TransCreate(trans *model.Transaction) (err error) {
-	d.do(func(db *database) {
-		err = db.C("transaction").Insert(trans)
+	err = d.do(func(db mongo.DB) error {
+		return db.C("transaction").Insert(trans)
 	})
 
 	if err != nil && mgo.IsDup(err) {
@@ -59,66 +64,70 @@ func (d *Dao) TransCreate(trans *model.Transaction) (err error) {
 }
 
 func (d *Dao) TransStepAdd(transUniqId string, step *model.TransactionStep) (err error) {
-	d.do(func(db *database) {
-		err = db.C("transaction").Update(bson.D{
-			{Name: "_id", Value: transUniqId },
-			{Name: "status", Value: model.TransactionStatusInit },
+	err = d.do(func(db mongo.DB) error {
+		return db.C("transaction").Update(bson.D{
+			{Name: "_id", Value: transUniqId},
+			{Name: "status", Value: model.TransactionStatusInit},
 		},
-		bson.D{
-			{Name: "$addToSet", Value: bson.D{ {Name: "steps", Value: step} }},
-			{Name: "$set", Value: bson.D{ {Name: "lu_time", Value: time.Now()} }},
-		})
+			bson.D{
+				{Name: "$addToSet", Value: bson.D{{Name: "steps", Value: step}}},
+				{Name: "$set", Value: bson.D{{Name: "lu_time", Value: time.Now()}}},
+			})
 	})
 	if err == mgo.ErrNotFound {
-		trans, _ := d.TransGet(transUniqId)
+		trans, errDB := d.TransGet(transUniqId)
 		if trans != nil {
 			err = errors.Coded(int32(contract.TryStepResponse_TransactionStatusError), fmt.Sprintf("Transaction status is %d", trans.Status))
-		} else {
+		} else if errDB == mgo.ErrNotFound {
 			err = errors.Coded(int32(contract.TryStepResponse_TransactionNotFound), err.Error())
+		} else {
+			err = errDB
 		}
 	}
 	return
 }
 
 func (d *Dao) TransConfirm(transUniqId string) (err error) {
-	d.do(func(db *database) {
-		err = db.C("transaction").Update(bson.D{
-			{Name: "_id", Value: transUniqId },
-			{Name: "status", Value: model.TransactionStatusInit },
+	err = d.do(func(db mongo.DB) error {
+		return db.C("transaction").Update(bson.D{
+			{Name: "_id", Value: transUniqId},
+			{Name: "status", Value: model.TransactionStatusInit},
 		},
-		bson.D{
-			{Name: "status", Value: model.TransactionStatusCommitted },
-			{Name: "$set", Value: bson.D{ {Name: "lu_time", Value: time.Now()} }},
-		})
+			bson.D{
+				{Name: "status", Value: model.TransactionStatusCommitted},
+				{Name: "$set", Value: bson.D{{Name: "lu_time", Value: time.Now()}}},
+			})
 	})
 	if err == mgo.ErrNotFound {
-		trans, _ := d.TransGet(transUniqId)
+		trans, errDB := d.TransGet(transUniqId)
 		if trans != nil {
 			if trans.Status == model.TransactionStatusCommitted {
 				err = nil
 			} else {
 				err = errors.Coded(int32(contract.ConfirmTransResponse_TransactionStatusError), fmt.Sprintf("Transaction status is %d", trans.Status))
 			}
-		} else {
+		} else if errDB == mgo.ErrNotFound {
 			err = errors.Coded(int32(contract.ConfirmTransResponse_TransactionNotFound), err.Error())
+		} else {
+			err = errDB
 		}
 	}
 	return
 }
 
 func (d *Dao) TransCancel(transUniqId string) (err error) {
-	d.do(func(db *database) {
-		err = db.C("transaction").Update(bson.D{
-			{Name: "_id", Value: transUniqId },
-			{Name: "status", Value: model.TransactionStatusInit },
+	err = d.do(func(db mongo.DB) error {
+		return db.C("transaction").Update(bson.D{
+			{Name: "_id", Value: transUniqId},
+			{Name: "status", Value: model.TransactionStatusInit},
 		},
 			bson.D{
-				{Name: "status", Value: model.TransactionStatusRollingBack },
-				{Name: "$set", Value: bson.D{ {Name: "lu_time", Value: time.Now()} }},
+				{Name: "status", Value: model.TransactionStatusRollingBack},
+				{Name: "$set", Value: bson.D{{Name: "lu_time", Value: time.Now()}}},
 			})
 	})
 	if err == mgo.ErrNotFound {
-		trans, _ := d.TransGet(transUniqId)
+		trans, errDB := d.TransGet(transUniqId)
 		if trans != nil {
 			if trans.Status == model.TransactionStatusRollingBack ||
 				trans.Status == model.TransactionStatusRolledBack {
@@ -126,66 +135,71 @@ func (d *Dao) TransCancel(transUniqId string) (err error) {
 			} else {
 				err = errors.Coded(int32(contract.CancelTransResponse_TransactionStatusError), fmt.Sprintf("Transaction status is %d", trans.Status))
 			}
-		} else {
+		} else if errDB == mgo.ErrNotFound {
 			err = errors.Coded(int32(contract.CancelTransResponse_TransactionNotFound), err.Error())
+		} else {
+			err = errDB
 		}
 	}
 	return
 }
 
 func (d *Dao) TransCancelSuccess(transUniqId string) (err error) {
-	d.do(func(db *database) {
-		err = db.C("transaction").Update(bson.D{
-			{Name: "_id", Value: transUniqId },
-			{Name: "status", Value: model.TransactionStatusRollingBack },
+	err = d.do(func(db mongo.DB) error {
+		return db.C("transaction").Update(bson.D{
+			{Name: "_id", Value: transUniqId},
+			{Name: "status", Value: model.TransactionStatusRollingBack},
 		},
 			bson.D{
-				{Name: "status", Value: model.TransactionStatusRolledBack },
-				{Name: "$set", Value: bson.D{ {Name: "lu_time", Value: time.Now()} }},
+				{Name: "status", Value: model.TransactionStatusRolledBack},
+				{Name: "$set", Value: bson.D{{Name: "lu_time", Value: time.Now()}}},
 			})
 	})
 	if err == mgo.ErrNotFound {
-		trans, _ := d.TransGet(transUniqId)
+		trans, errDB := d.TransGet(transUniqId)
 		if trans != nil {
 			if trans.Status == model.TransactionStatusRolledBack {
 				err = nil
 			} else {
 				err = errors.Coded(int32(contract.CancelTransSuccessResponse_TransactionStatusError), fmt.Sprintf("Transaction status is %d", trans.Status))
 			}
-		} else {
+		} else if errDB == mgo.ErrNotFound {
 			err = errors.Coded(int32(contract.CancelTransSuccessResponse_TransactionNotFound), err.Error())
+		} else {
+			err = errDB
 		}
 	}
 	return
 }
 
 func (d *Dao) TransGetExpiredList(topN int32) (transUniqIds []string, err error) {
-	d.do(func(db *database) {
+	err = d.do(func(db mongo.DB) error {
 		transUniqIds = []string{}
 		transList := make([]*model.Transaction, 0)
 		q := bson.M{
-			"expire_time": bson.M{ "$lt": time.Now()},
-			"status": model.TransactionStatusInit,
+			"expire_time": bson.M{"$lt": time.Now()},
+			"status":      model.TransactionStatusInit,
 		}
-		s := bson.M{ "_id": 1 }
-		err = db.C("transaction").Find(q).Select(s).
+		s := bson.M{"_id": 1}
+		errDB := db.C("transaction").Find(q).Select(s).
 			Sort("_id").Limit(int(topN)).All(&transList)
-		if err == nil {
+		if errDB == nil {
 			for _, trans := range transList {
 				transUniqIds = append(transUniqIds, trans.ID.String())
 			}
 		}
+		return errDB
 	})
 	return
 }
 
 func (d *Dao) TransGetRollingBackList(topN int32) (transList []*model.Transaction, err error) {
-	d.do(func(db *database) {
+	err = d.do(func(db mongo.DB) error {
 		transList = []*model.Transaction{}
 		q := bson.M{
 			"status": model.TransactionStatusRollingBack,
 		}
-		err = db.C("transaction").Find(q).Sort("_id").
+		return db.C("transaction").Find(q).Sort("_id").
 			Limit(int(topN)).All(&transList)
 	})
 	return
